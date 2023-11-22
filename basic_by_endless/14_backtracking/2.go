d6f63bce67211f7091ecedcfe4550214fd072ef9
张春雷 <zhangchunlei@qiyi.com>
2023-11-22 18:29:42 +0800

    chores

--- basic_by_endless/14_backtracking/2.go
+++ basic_by_endless/14_backtracking/2.go
@@ -1,5 +1,6 @@
 package backtracking
 
+// 这种写法要比第一种写法难
 // 第一遍就写错了， 再练
 // 从 answer 的视角 (选哪个数）
 func subsets(nums []int) [][]int {
@@ -10,11 +11,8 @@ func subsets(nums []int) [][]int {
 
 	n := len(nums)
 	dfs = func(i int) {
-		a := make([]int, len(path))
-		copy(a, path)
-		ans = append(ans, a)
-
-		// 技巧1： ans = append(ans, append([]int(nil), path...)) // copy path
+		// add to ans
+		ans = append(ans, append([]int(nil), path...))
 
 		if i == n {
 			return
@@ -23,13 +21,22 @@ func subsets(nums []int) [][]int {
 		for j := i; j < n; j++ {
 			path = append(path, nums[j])
 			dfs(j + 1)
-			l := len(path)
-			path = path[:l-1]
-
-			// 技巧 2 path = path[:len(path)-1]
+			path = path[:len(path)-1]
 		}
 	}
 
 	dfs(0)
 	return ans
+}
+
+func dummy() {
+	// 不好的示范
+	//a := make([]int, len(path))
+	//copy(a, path)
+	//ans = append(ans, a)
+	// 技巧1： ans = append(ans, append([]int(nil), path...)) // copy path
+
+	//l := len(path)
+	//path = path[:l-1]
+	// 技巧 2 path = path[:len(path)-1]
 }
