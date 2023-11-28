2d32e62f2d7ba31f5f090cd77ee2cc9967a593e7
张春雷 <zhangchunlei@qiyi.com>
2023-11-28 11:39:14 +0800

    adding sieve of eratosthenes example

--- sieveofetratosthenes/example/main.go
+++ sieveofetratosthenes/example/main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/bbs731/algorithms/sieveofetratosthenes"
+	"sieveofetratosthenes"
 	"time"
 )
 
-
 func main() {
-	start := time.Now() // 开始时间
+	var start time.Time
+	var end time.Duration
+
+	start = time.Now() // 开始时间
 	fmt.Println("CountPrimes")
 	fmt.Printf("answer is : %d\n", sieveofetratosthenes.CountPrimes(10000000))
-	end := time.Since(start)
+	end = time.Since(start)
 	fmt.Printf("total time: %s\n", end)
 
 	start = time.Now() // 开始时间
 	fmt.Println("CountPrimesSieve")
 	fmt.Printf("answer is : %d\n", sieveofetratosthenes.CounterPrimesSieve(10000000))
-	end := time.Since(start)
+	end = time.Since(start)
 	fmt.Printf("total time: %s\n", end)
 
 }
