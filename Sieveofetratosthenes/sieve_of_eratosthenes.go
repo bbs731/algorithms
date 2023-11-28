3b265d46fa0456339340cc863373df655222d86d
张春雷 <zhangchunlei@qiyi.com>
2023-11-28 11:57:45 +0800

    sieve_of_eratosthenes 的时间复杂度计算估计

--- Sieveofetratosthenes/sieve_of_eratosthenes.go
+++ Sieveofetratosthenes/sieve_of_eratosthenes.go
@@ -3,6 +3,17 @@ package Sieveofetratosthenes
 /*
 非常清楚的讲解，和数学证明
 https://www.cnblogs.com/pxsong/p/SieveofEratosthenes.html
+
+1 + 1/2 + 1/3 + 1/4 + ..... 1/n  = O(ln(n))
+
+1/2 + 1/3 + 1/5 + 1/7 + 1/11 + ..... + 1/p  其中 p 为质数。 质数的分布函数是欧拉乘积公式给的。
+可以这样理解吗？ 质数在自然数中是近似按照 log 函数分布的
+
+Sum(1/p) = ln( sum(1/n)) = ln(ln(n))
+
+所以 SieveOfEratosthenes 的时间复杂度是：
+n*(Sum(1/p)) = n *ln(ln(n))
+
  */
 
 func isPrime(n int) bool {
