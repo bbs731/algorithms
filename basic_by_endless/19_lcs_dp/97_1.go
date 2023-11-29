9d3722fb44f2f8d2e68524f507e27f835228d621
unknown <zhangchunlei@qiyi.com>
2023-11-29 22:22:48 +0800

    adding dp solution for LC97

--- basic_by_endless/19_lcs_dp/97_1.go
+++ basic_by_endless/19_lcs_dp/97_1.go
@@ -1,7 +1,76 @@
 package dp
 
 
-func isInterleave(s1 string, s2 string, s3 string) bool {
+
+/*
+	f[i+1][j+1] = f[i][j+1] || f[i+1][j]
+	f[j+1] = f[j+1] || f[j]
+ */
+func isInterleave(s1 string, s2 string, s3 string) bool{
+	n :=len(s1)
+	m := len(s2)
+	if n + m != len(s3) {
+		return false
+	}
+
+	f := make([]bool, m+1)
+	for j:=0; j<=m; j++ {
+		f[j] = s2[:j] == s3[:j]
+	}
+
+	for i:=0; i<n; i++ {
+		f[0] = s1[:i+1] == s3[:i+1]
+		for j:=0; j<m; j++ {
+			var a , b bool
+			if  s1[i] == s3[i+j+1] {
+				a =f[j+1]
+			}
+			if s2[j]== s3[i+j+1]{
+				b = f[j]
+			}
+			f[j+1] = a || b
+		}
+	}
+	return f[m]
+}
+
+/*
+	f[i][j] =  f[i-1][j] || f[i][j-1]
+
+	f[i+1][j+1] = f[i][j+1] || f[i+1][j]
+ */
+func isInterleave_dp(s1 string, s2 string, s3 string) bool{
+	n :=len(s1)
+	m := len(s2)
+	if n + m != len(s3) {
+		return false
+	}
+
+	f := make([][]bool, n+1)
+	for i:=0; i<=n; i++ {
+		f[i]= make([]bool, m+1)
+		f[i][0] = s1[:i] == s3[:i]
+		for j:=0; j<=m; j++{
+			f[0][j]	= s2[:j] == s3[:j]
+		}
+	}
+
+	for i:=0; i<n; i++ {
+		for j:=0; j<m; j++ {
+			var a , b bool
+			if  s1[i] == s3[i+j+1] {
+				a = f[i][j+1]
+			}
+			if s2[j]== s3[i+j+1]{
+				b = f[i+1][j]
+			}
+			f[i+1][j+1] = a || b
+		}
+	}
+	return f[n][m]
+}
+
+func isInterleave_dfs(s1 string, s2 string, s3 string) bool {
 	n :=len(s1)
 	m := len(s2)
 	if n + m != len(s3) {
