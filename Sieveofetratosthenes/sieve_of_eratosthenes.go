739119c3a1dc57d156ce3d5d6ee33375f8b1bd59
张春雷 <zhangchunlei@qiyi.com>
2023-11-29 09:16:22 +0800

    correct the sieve

--- Sieveofetratosthenes/sieve_of_eratosthenes.go
+++ Sieveofetratosthenes/sieve_of_eratosthenes.go
@@ -41,7 +41,7 @@ func CounterPrimesSieve(n int) int {
 		primes[i] = true
 	}
 
-	for i := 2; i < n; i++ {
+	for i := 2; i*i < n; i++ {
 		if primes[i] {
 			for j := i * i; j < n; j += i {
 				primes[j] = false
