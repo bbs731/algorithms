6bf50cbe1678a0a03b9857a60eb6f969bfc9d1e0
张春雷 <zhangchunlei@qiyi.com>
2023-12-05 11:32:35 +0800

    commit a stupid version

---
+++ basic_by_endless/20_LIS_dp/673.go
@@ -0,0 +1,40 @@
+package dp
+
+// f[i] = max(f[j]) + 1
+//[1,2,4,3,5,4,7,2]
+// [1,2,3,1,2,3,1,2,3] 这个测试用例绝绝子。
+// 这道题，充分的说明了你的脑子不是很好使
+func findNumberOfLIS(nums []int) int {
+	ans := 0
+	longest := 1
+	f := make([]int, len(nums))
+	t := make([]int, len(nums))
+
+	for i, x := range nums {
+		f[i] = 1
+		t[i] = 0
+		local := 0
+		for j := 0; j < i; j++ {
+			if x > nums[j] {
+				f[i] = max(f[i], f[j]+1)
+				if f[j]+1 > local {
+					local = f[i]
+					t[i] = t[j]
+				} else if f[j]+1 == local {
+					t[i] += t[j]
+				}
+			}
+			longest = max(longest, f[i])
+		}
+		if t[i] == 0 {
+			t[i] = 1
+		}
+	}
+
+	for i := range nums {
+		if f[i] == longest {
+			ans += t[i]
+		}
+	}
+	return ans
+}
