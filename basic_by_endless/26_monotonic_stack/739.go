66f7e4fbe76eff4d6072f13f32f40aba70f325bb
chunlei <zhang_chunlei@hotmail.com>
2023-12-10 16:43:13 +0800

    adding more terse code fro 739

--- basic_by_endless/26_monotonic_stack/739.go
+++ basic_by_endless/26_monotonic_stack/739.go
@@ -1,5 +1,24 @@
 package monotonic_stack
 
+
+func dailyTemperatures(temperatures []int) []int {
+	n := len(temperatures)
+	stack := make([]int, 0)  //monotoinic stack 里面放的是 index
+	ans := make([]int, n)
+
+	for i, x := range temperatures {
+		for len(stack)> 0 && x > temperatures[stack[len(stack)-1]] {
+			top := stack[len(stack)-1]
+			ans[top] = i - top
+			// pop top
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+	return ans
+}
+
+
 func dailyTemperatures(temperatures []int) []int {
 	n := len(temperatures)
 	stack := make([]int, 0)  //monotoinic stack 里面放的是 index
@@ -19,6 +38,5 @@ func dailyTemperatures(temperatures []int) []int {
 		// push x
 		stack = append(stack, i)
 	}
-	ans[n-1]= 0
 	return ans
 }
