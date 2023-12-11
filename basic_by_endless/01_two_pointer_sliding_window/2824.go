13c7c62584c9d8c1dc7bf1ac1a275dcb811646be
张春雷 <zhangchunlei@qiyi.com>
2023-12-11 11:10:02 +0800

    2824 2 pointers sliding window

---
+++ basic_by_endless/01_two_pointer_sliding_window/2824.go
@@ -0,0 +1,20 @@
+package sliding_window
+
+func countPairs(nums []int, target int) int {
+	sort.Ints(nums)
+	n := len(nums)
+	cnt := 0
+
+	left := 0
+	right := n - 1
+	for left < right {
+		sum := nums[left] + nums[right]
+		if sum < target {
+			cnt += right - left // 这里犯了个错误，不是 cnt++, 这里的计数是个技巧！
+			left++
+		} else {
+			right--
+		}
+	}
+	return cnt
+}
