a3d24f1bd98b50cd2135706ab60ed71c2aa87f40
张春雷 <zhangchunlei@qiyi.com>
2023-12-12 15:26:54 +0800

    chores

--- basic_by_endless/09_binary_tree_dfs_recursion/111.go
+++ basic_by_endless/09_binary_tree_dfs_recursion/111.go
@@ -43,16 +43,8 @@ func minDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-
-	left := minDepth(root.Left)
-	right := minDepth(root.Right)
-
-	if left == 0 {
-		return right + 1
+	if root.Left == nil || root.Right == nil {
+		return max(minDepth(root.Left), minDepth(root.Right)) + 1
 	}
-	if right == 0 {
-		return left + 1
-	}
-
-	return min(left, right) + 1
+	return min(minDepth(root.Left), minDepth(root.Right)) + 1
 }
