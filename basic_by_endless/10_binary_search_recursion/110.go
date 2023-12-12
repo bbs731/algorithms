346a945c288b72f48aaa9850b720dc231502227a
张春雷 <zhangchunlei@qiyi.com>
2023-12-12 18:35:12 +0800

    无聊 110

--- basic_by_endless/10_binary_search_recursion/110.go
+++ basic_by_endless/10_binary_search_recursion/110.go
@@ -1,3 +1,25 @@
 package binary_tree
 
+/*
+好无聊，好无聊!
+ */
+func abs(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+func isBalanced(root *TreeNode) bool {
 
+	var dfs func(*TreeNode) (bool, int)
+	dfs = func(node *TreeNode) (bool, int) {
+		if node == nil {
+			return true, 0
+		}
+		lb, l := dfs(node.Left)
+		rb, r := dfs(node.Right)
+		return lb && rb && abs(l, r) <= 1, max(l, r) + 1
+	}
+	ok, _ := dfs(root)
+	return ok
+}
