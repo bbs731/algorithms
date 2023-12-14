340100cf1de8c0af74877d6b5937dfd88d6bd1ad
张春雷 <zhangchunlei@qiyi.com>
2023-12-14 13:41:39 +0800

    260 bit manupiltaion

--- one-day-exercise/260.go
+++ one-day-exercise/260.go
@@ -6,8 +6,8 @@ func singleNumber(nums []int) []int {
 		total ^= x
 	}
 	// find the last bit
-	lowbit := total & (^total + 1) // x &(^x +1) 取最后一个是1的bit
-	//lowbit := total & (-total) // -total = ^total + 1  你注意到这一点了吗？
+	//lowbit := total & (^total + 1) // x &(^x +1) 取最后一个是1的bit
+	lowbit := total & (-total) // -total = ^total + 1  你注意到这一点了吗？
 
 	one, two := 0, 0
 	for _, x := range nums {
