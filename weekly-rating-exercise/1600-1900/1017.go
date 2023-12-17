430d39e015a2e2f4cd81f9a1c1d0fefa009ee8b2
chunlei <zhang_chunlei@hotmail.com>
2023-12-17 16:14:59 +0800

    adding 1017 offical solution

--- weekly-rating-exercise/1600-1900/1017.go
+++ weekly-rating-exercise/1600-1900/1017.go
@@ -1,68 +1,31 @@
 package _600_1900
 
-/*
-烂题，搞不定！
 
-下面是错误的答案
- */
-func lowest_neg_number (num int) (int, int){
-	base := -2
-	cnt := 1
-	for num < base {
-		base = base *4
-		cnt += 2
-	}
-	return cnt, base
-}
-func lowest_base_number(num int) (int, int) {
-	base := 1
-	cnt := 0
-	for num > base {
-		base = base *2
-		cnt++
-	}
-	return cnt, base
-}
 func baseNeg2(n int) string {
-	var ans string
 	if n == 0 {
 		return "0"
 	}
-	content := make([]byte, 34)
-	for ;;{
-		ep, eb :=lowest_base_number(n)
-		if n == eb {
-			content[ep]= 1
-			break
-		}
-
-		if ep %2 == 1 {
-			content[ep-1]	= 1
-			n = n- eb/2
-		} else {
-			content[ep]	= 1
-			n = n-eb
-			op, ob := lowest_neg_number(n)
-			content[op]= 1
-			if ob == n {
-				break
-			}
-			n = n - ob
-		}
+	if n ==1 {
+		return "1"
 	}
 
-	started := false
-	for i := len(content)-1; i>=0; i-- {
-		if content[i] == 1 {
-			started = true
-		}
-		if started {
-			if content[i] == 1 {
-				ans += "1"
-			} else {
-				ans += "0"
-			}
-		}
+	res := make([]byte, 0)
+
+	/*  左右进制的原理， 都是一样的。
+	https://leetcode.cn/problems/convert-to-base-2/solutions/2209807/fu-er-jin-zhi-zhuan-huan-by-leetcode-sol-9qlh/
+
+	看官方的题解!`
+	 */
+	for n !=0 {
+		remainder := n &1
+		res = append(res, byte('0' + remainder))
+		n -= remainder
+		n /=-2
 	}
-	return ans
+
+	l := len(res)
+	for i:=0; i<= len(res)/2; i++ {
+		res[i], res[l-1-i] = res[l-1-i], res[i]
+	}
+	return string(res)
 }
