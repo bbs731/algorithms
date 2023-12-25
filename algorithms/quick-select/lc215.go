ccd391f1b04a3cc1e838495baa6b2943569e1d5d
张春雷 <zhangchunlei@qiyi.com>
2023-12-25 19:48:56 +0800

    adding some comments

--- algorithms/quick-select/lc215.go
+++ algorithms/quick-select/lc215.go
@@ -5,8 +5,11 @@ func findKthLargest(nums []int, k int) int {
 	return quickselect_loop(nums, 0, n-1, n-k)
 }
 
-// 写成左闭右开是对的， 为啥写成闭区间是错的？
-// recursive to loop 怎么选择区间？
+// 写成左闭右开是对的， 为啥写成闭区间是错的？  我感觉就是碰巧了， 在双指针的 loop 里 有 j := r +1  只适合 [ ) 左闭右开区间
+// 为什么 闭区间不行， 把 partition 写成标准的样子，闭区间写是没有问题的， 但是，partition 里面用了双指针的技巧，造成，写闭区间不行。
+// 因为有 i := l - 1 和 j := r+ 1 最后造成返回下标，有的时候 返回i 正确，有的时候返回 j 正确，无法判断。 但是标准的 partition 版本，
+// 只move 一个指针，写闭区间，正确性是没问题的，但是这道 215的题会超时。
+// recursive to loop 怎么选择区间？ 这道题是碰巧不要深究。
 
 func quickselect_loop(nums []int, l, r, k int) int {
 	for l < r { // [l, r)  左闭右开区间
@@ -25,7 +28,7 @@ func quickselect_loop(nums []int, l, r, k int) int {
 			}
 		}
 		if k <= j {
-			r = j
+			r = j // loop 里正好有一个  j := r+1  所以天然的凑巧适合了左闭右开区间
 		} else {
 			//return quickselect(nums, j+1, r, k)
 			l = j + 1
