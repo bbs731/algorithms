7d19f408cc269c67b34682654a68ff9675bc0f8a
张春雷 <zhangchunlei@qiyi.com>
2023-12-28 16:25:03 +0800

    find lca of binary tree

--- basic_by_endless/12_binary_search_lca/236.go
+++ basic_by_endless/12_binary_search_lca/236.go
@@ -1,1 +1,29 @@
-package biner
+package binary_tree_lca
+
+/*
+ 	这种题，太容易出错了， 一遍写不对啊！
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left != nil {
+		return left
+	}
+	return right
+}
