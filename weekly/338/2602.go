b3c504b7e908fa9ef21c0cd913b5b49a6921b096
chunlei <zhang_chunlei@hotmail.com>
2024-01-02 23:59:35 +0800

    chores

--- weekly/338/2602.go
+++ weekly/338/2602.go
@@ -12,16 +12,20 @@ func minOperations(nums []int, queries []int) []int64 {
 	}
 
 	for i, q := range queries {
-		pos := sort.SearchInts(nums, q)
-		if pos == n  {
-			ans[i] = int64(n*q - (presum[n]-presum[0]))
-		} else if pos == 0 {
-			ans[i] = int64(presum[n]-presum[0] - n*q)
-		} else{
-			part1 := pos*q - (presum[pos] - presum[0])
-			part2 := presum[n] - presum[pos] - (n-pos)*q
-			ans[i] = int64(part1 + part2)
-		}
+		pos := sort.SearchInts(nums, q)  // 你还是不了解二分， SearchInts 返回的index是什么，理解错误！
+		//if pos == n  {
+		//	ans[i] = int64(n*q - (presum[n]-presum[0]))
+		//} else if pos == 0 {
+		//	ans[i] = int64(presum[n]-presum[0] - n*q)
+		//} else{
+		//	part1 := pos*q - (presum[pos] - presum[0])
+		//	part2 := presum[n] - presum[pos] - (n-pos)*q
+		//	ans[i] = int64(part1 + part2)
+		//}
+		// 可以简化成这样！
+		part1 := pos*q - (presum[pos] - presum[0])
+		part2 := presum[n] - presum[pos] - (n-pos)*q
+		ans[i] = int64(part1 + part2)
 	}
 	return ans
 }
