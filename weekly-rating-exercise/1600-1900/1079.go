c130c4be08d9444b2ed0a49f8df38ef7cc71e851
chunlei <zhang_chunlei@hotmail.com>
2024-01-07 00:28:16 +0800

    1079, silly solution

--- weekly-rating-exercise/1600-1900/1079.go
+++ weekly-rating-exercise/1600-1900/1079.go
@@ -1,1 +1,40 @@
 package _600_1900
+
+
+
+// 看看 灵神的题解， O（n^2) 的解法
+//https://leetcode.cn/problems/letter-tile-possibilities/solutions/2275356/on2-ji-shu-dppythonjavacgo-by-endlessche-hmez/
+//状态转移方程，很难定义啊！
+
+
+
+
+// dfs 过，是因为 len(tiles) <=7  再大就过不去了。
+func numTilePossibilities(tiles string) int {
+	n := len(tiles)
+	ans := 0
+	tables := make(map[string]bool)
+	tables[""] = true
+
+	var dfs func(int, string)
+	dfs = func(i int, content string){
+		if i == n {
+			if _, ok := tables[content]; !ok {
+				ans++
+				tables[content]= true
+			}
+			return
+		}
+
+		c := string(tiles[i])
+		// 不选
+		dfs(i+1, content)
+		dfs(i+1, c+content)
+		dfs(i+1, content+c)
+		for j :=1; j<len(content); j++ {
+			dfs(i+1, content[:j] + string(tiles[i]) + content[j:])
+		}
+	}
+	dfs(0, "")
+	return ans
+}
--- weekly-rating-exercise/1600-1900/823.go
+++ weekly-rating-exercise/1600-1900/823.go
@@ -1,1 +1,32 @@
 package _600_1900
+
+import "sort"
+
+func numFactoredBinaryTrees(arr []int) int {
+	mod := int(1e9 + 7)
+	n := len(arr)
+	sort.Ints(arr)
+	dp := make([]int, n)
+	h := make(map[int]int, n)
+	for i, x := range arr {
+		h[x]=i
+		dp[i] = 1
+	}
+	ans :=1
+	for i:=1; i<n; i++ {
+		for j:=0; j<=i-1;j++ {
+			a := arr[j]
+			b := arr[i]/a
+			bi, ok := h[b]
+			if a * b == arr[i] && ok {
+				dp[i] = (dp[i] + dp[j]*dp[bi])%mod
+				// loop from 0 to i-1 , a != b 自然会被 + 两遍
+				//if a != b {
+				//	dp[i] = (dp[i] + dp[j]*dp[bi])%mod
+				//}
+			}
+		}
+		ans = (ans+dp[i])%mod
+	}
+	return ans
+}
