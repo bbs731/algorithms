0ba385722225724e6931fc95b3372df1f17ba705
chunlei <zhang_chunlei@hotmail.com>
2024-01-07 23:41:07 +0800

    chores

--- weekly/379/d.go
+++ weekly/379/d.go
@@ -6,49 +6,50 @@ import "math/bits"
 
 func maxPartitionsAfterOperations(s string, k int) int {
 	n := len(s)
-
 	type args struct {
-		pos, mask int
+		i, mask int
 		changed bool
 	}
-	memo := make(map[args]int)
-
-	var dfs func(int, int , bool) int
-	dfs = func(i int, mask int, changed bool) (res int) {
+	memo := map[args]int{}
+	var dfs func(int, int, bool) int
+	dfs = func(i, mask int, changed bool) (res int) {
 		if i == n {
-			return 0
+			return 1
 		}
 
 		a := args{i, mask, changed}
-		if v, ok := memo[a]; ok {
+		if v, ok := memo[a]; ok { // 之前计算过
 			return v
 		}
 
-		bit := 1 << (s[i]- 'a')
+		// 不改 s[i]
+		bit := 1 << (s[i] - 'a')
 		newMask := mask | bit
-		// no change bit
 		if bits.OnesCount(uint(newMask)) > k {
-			res =  dfs(i+1, bit, changed) + 1
-		}else {
-			res =  dfs(i+1, newMask, changed)
+			// 分割出一个子串，这个子串的最后一个字母在 i-1
+			// s[i] 作为下一段的第一个字母，也就是 bit 作为下一段的 mask 的初始值
+			res = dfs(i+1, bit, changed) + 1
+		} else { // 不分割
+			res = dfs(i+1, newMask, changed)
 		}
 
-		//change bit
 		if !changed {
-			// loop the char to be changed
-			for j:=0; j<26; j++{
-				newMask := mask | 1<< j
+			// 枚举把 s[i] 改成 a,b,c,...,z
+			for j := 0; j < 26; j++ {
+				newMask := mask | 1<<j
 				if bits.OnesCount(uint(newMask)) > k {
-					res = max(res, dfs(i+1, 1 <<j, true)+1)
-				}else {
+					// 分割出一个子串，这个子串的最后一个字母在 i-1
+					// j 作为下一段的第一个字母，也就是 1<<j 作为下一段的 mask 的初始值
+					res = max(res, dfs(i+1, 1<<j, true)+1)
+				} else { // 不分割
 					res = max(res, dfs(i+1, newMask, true))
 				}
 			}
 		}
 
-		memo[a]= res
+		memo[a] = res // 记忆化
 		return res
 	}
-
-	return dfs(0,0, false)
+	return dfs(0, 0, false)
 }
+
