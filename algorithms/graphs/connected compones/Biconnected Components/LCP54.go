88a990b84a9dbfebf3ea8b166df886c69897436f
chunlei <zhang_chunlei@hotmail.com>
2024-01-13 15:55:08 +0800

    addign BCC template question example LCP54

--- algorithms/graphs/connected compones/Biconnected Components/LCP54.go
+++ algorithms/graphs/connected compones/Biconnected Components/LCP54.go
@@ -1,9 +1,13 @@
+package Biconnected_Components
+
+import "sort"
+
 package one_day_exercise
 
 import (
-	"fmt"
-	"slices"
-	"sort"
+"fmt"
+"slices"
+"sort"
 )
 
 
@@ -13,11 +17,11 @@ import (
 // 我考虑清楚了， 需要找到 所有的 bcc 然后取, 前  len(bcc) -1 个 bcc componenet,  每个 bcc componenet 里取最小的 vertex cost  然后相加。
 // 思路是对的， 但是，犯了几个错误。
 // 1. 没区分 scc 和 bcc 的区别， 这个，需要再学习bcc, 因为之前没写过 bcc 的代码, 这个知识点拉掉了。
-// 2. 像题解里说的。 不是每个 bcc 都要参与到最后的计算当中的， 只有哪些，缩点之后是叶子节点的 bcc 在需要考虑在最后的答案计算当中的。 （想想为什么？）
+// 2. 像题解里说的。 不是每个 bcc 都要参与到最后的计算当中的， 只有哪些，缩点之后是叶子节点的 bcc 在需要考虑在最后的答案计算当中的。 （想想为什么？ 链接 2个 或者两个以上 cut-vertex 的 bcc 缩点之后，就是non-leaf 节点。）
 // 3. 如何知道一个 bcc 缩点之后是叶子节点， （题目作的时候不需要真的缩点） 当， bcc 中所有的 vertex 中，有且只有一个 cut vertex 那么，这个 bcc 就是缩点之后的一个叶子节点。
 // 4. bcc 的代码，来自 灵神的代码库 codeforeces-go/graph.go  还没读过。 抓紧。
 
-// 另外， 这真是一个好题。  图根据 bcc 缩点之后，变成一个数， 考虑所有的树的叶子节点， 然后把 排名 n-1 之前的叶子节点的 cost 都加起来就是的答案。 多么好的题啊。
+// 另外， 这真是一个好题。  图根据 bcc 缩点之后，变成一颗树. 考虑所有的树的叶子节点， 然后把 排名 n-1 之前的叶子节点的 cost 都加起来就是的答案。 多么好的题啊。
 func findVertexBCC(g [][]int) (comps [][]int,  isCut[]bool) {
 	bccIDs := make([]int, len(g)) // ID 从 1 开始编号
 	idCnt := 0
@@ -118,7 +122,7 @@ func minimumCost(cost []int, roads [][]int) int64 {
 			if isCut[x] {
 				cnt++
 			} else {
-					mn = min(mn, cost[x])
+				mn = min(mn, cost[x])
 			}
 		}
 		if cnt == 1 {
