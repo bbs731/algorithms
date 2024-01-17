8650d9a8f5ef9c7861d4a61431cc9a019931e7a5
张春雷 <zhangchunlei@qiyi.com>
2024-01-17 16:02:23 +0800

    update codeforce problem 1909c

--- main/1900-1999/1909C.go
+++ main/1900-1999/1909C.go
@@ -1,1 +1,89 @@
-package _900_1999
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+	"sort"
+)
+
+func cf1909C(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var T, n int
+	//var l, r, c []int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n)
+		l := make([]int, n)
+		for i := range l {
+			Fscan(in, &l[i])
+		}
+		r := make([]int, n)
+		for i := range r {
+			Fscan(in, &r[i])
+		}
+
+		c := make([]int, n)
+		for i := range c {
+			Fscan(in, &c[i])
+		}
+
+		ans := 0
+		sort.Ints(c)
+		sort.Ints(l)
+		sort.Ints(r)
+		type pair struct {
+			s, e int // 区间的两个端点
+		}
+		pairs := make([]pair, 0)
+
+		// a sliding window
+		// 这里  match intervals 的想法是错误的。
+		// 如何 match interval 的左右端点？ 让 e - s 尽量的大， 还能满足，最优能 n paired.
+
+		// 如何去 match interval 答案里说， 这和 bracket matching  '('  和 ')' 配对是一个问题。
+		// 灵神用了一个 stack 来实现。
+		st := make([]int, 0)
+		j := 0
+		for _, v := range r {
+			for j < n && l[j] < v {
+				st = append(st, l[j])
+				j++
+			}
+			// v and top of stack form a pair
+			pairs = append(pairs, pair{st[len(st)-1], v})
+			// pop stack
+			st = st[:len(st)-1]
+		}
+		//right := n - 1
+		//for right >= 0 {
+		//	left := right - 1
+		//	for left >= 0 {
+		//		if r[left] > l[right] {
+		//			left--
+		//		} else {
+		//			break
+		//		}
+		//	}
+		//	j := right
+		//	for i := left + 1; i <= right; i++ {
+		//		pairs = append(pairs, pair{l[i], r[j]})
+		//		j--
+		//	}
+		//	right = j
+		//}
+
+		// 按照 Interval length 倒序排列
+		sort.Slice(pairs, func(i, j int) bool { return (pairs[i].e - pairs[i].s) > (pairs[j].e - pairs[j].s) })
+
+		for i := 0; i < n; i++ {
+			ans += c[i] * (pairs[i].e - pairs[i].s)
+		}
+		Fprintln(out, ans)
+	}
+}
+
+func main() { cf1909C(os.Stdin, os.Stdout) }
--- main/1900-1999/1909C_test.go
+++ main/1900-1999/1909C_test.go
@@ -1,1 +1,34 @@
-package _900_1999
+package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+// https://codeforces.com/problemset/problem/1909/C
+// https://codeforces.com/problemset/status/1909/problem/C
+func Test_cf1909C(t *testing.T) {
+	testCases := [][2]string{
+		{
+			`3
+2
+8 3
+12 23
+100 100
+4
+20 1 2 5
+30 4 3 10
+2 3 2 3
+10
+76042 155685 62534 162770 779 3495 97453 122787 86743 142857
+81292 172088 146526 199306 36432 165338 168285 127772 119677 151891
+1 8 2 9 1 1 1 4 7 4
+`,
+			`2400
+42
+609373`,
+		},
+	}
+	testutil.AssertEqualStringCase(t, testCases, 0, cf1909C)
+}
+
