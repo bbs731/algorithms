95edb08f77cde8226e974f6d2335a8d74763477b
chunlei <zhang_chunlei@hotmail.com>
2024-01-21 23:55:12 +0800

    chores

--- algorithms/sliding-window/多指针滑动窗口/2563.go
+++ algorithms/sliding-window/多指针滑动窗口/2563.go
@@ -14,12 +14,16 @@ import (
  这道题，做了3个小时吗？ 怎么办？
 
  lower_bound,
- upper_bound 还是心魔啊！
+ upper_bound 还是心魔啊！ upper_bound 的定义， 一值模棱两可， 你不是很确定， upper_bound 指的是那个位置。
+
+ https://stackoverflow.com/questions/28389065/difference-between-basic-binary-search-for-upper-bound-and-lower-bound
   */
 
 
 // 春雷你对 upper_bound 的理解，一直是有错误的。
+// https://stackoverflow.com/questions/28389065/difference-between-basic-binary-search-for-upper-bound-and-lower-bound
 
+// 找机会把 upper bound 的 几重实现， 再多写几遍！
 func upper_bound(nums []int, v int, r int) int {
 	left :=  -1
 	right := r
@@ -65,7 +69,7 @@ func countFairPairs(nums []int, lower int, upper int) int64 {
 		pos_right:= upper_bound(nums, upper-v, right)
 		//pos_right:= sort.SearchInts(nums[:right], upper-v+1)
 		pos_left := lower_bound(nums, lower -v, right)
-		ans += pos_right - pos_left
+		ans += pos_right - pos_left  // pos_right-1 - pos_left +1  = pos_right - pos_left
 	}
 	return int64(ans)
 }
