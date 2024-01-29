6bf5b9b40cba5843e3041236ea36e6d3010939fa
chunlei <zhang_chunlei@hotmail.com>
2024-01-29 22:14:10 +0800

    adding 84

--- data-structures/montonic_stack/矩形系列/84.go
+++ data-structures/montonic_stack/矩形系列/84.go
@@ -20,6 +20,39 @@ package monotonic_stack
 
  */
 
-func largestRectangleArea(heights []int) int {
+/***
 
+ 宫三的题解：
+ https://leetcode.cn/problems/largest-rectangle-in-histogram/solutions/1856193/by-ac_oier-i470/
+
+ 枚举每个柱子， 找到柱子的左右边界， 就是，左右第一个比柱子小的坐标。（这个不是单调栈的模版吗？）
+
+ 能想到，这么解的都是天才。
+ */
+
+
+func largestRectangleArea(heights []int) int {
+	n :=len(heights)
+	left := make([]int, n)
+	right := make([]int, n)
+	for i := range right {
+		right[i]= n
+	}
+
+	st := []int{-1}
+	for i, v := range heights {
+		for len(st)	 > 1 && v < heights[st[len(st)-1]] {
+			right[st[len(st)-1]] = i
+			// pop stack
+			st = st[:len(st)-1]
+		}
+		left[i] = st[len(st)-1]
+		st = append(st, i)
+	}
+
+	ans := 0
+	for i, v := range heights {
+		ans = max(ans, (right[i] - left[i]-1)*v)
+	}
+	return ans
 }
