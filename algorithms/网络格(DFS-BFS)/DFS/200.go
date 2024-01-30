8dcd64c20ee9f85a829738313861e6641e891eee
张春雷 <zhangchunlei@qiyi.com>
2024-01-30 13:02:02 +0800

    adding 200 template question

---
+++ algorithms/网络格(DFS-BFS)/DFS/200.go
@@ -0,0 +1,30 @@
+package DFS
+
+func numIslands(grid [][]byte) int {
+	m := len(grid)
+	n := len(grid[0])
+
+	var dfs func(int, int)
+	dfs = func(i, j int) {
+		if i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0' {
+			return
+		}
+		grid[i][j] = '0' // mark 成 visited
+		dir := [][2]int{{1, 0}, {-1, 0}, {0, -1}, {0, 1}}
+		for _, d := range dir {
+			dfs(i+d[0], j+d[1])
+		}
+		return
+	}
+
+	ans := 0
+	for i := range grid {
+		for j := range grid[0] {
+			if grid[i][j] == '1' {
+				dfs(i, j)
+				ans++
+			}
+		}
+	}
+	return ans
+}
