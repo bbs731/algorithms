24ccb23d674a815684e1f059fb2fa30a96fea18c
张春雷 <zhangchunlei@qiyi.com>
2024-01-31 09:35:31 +0800

    adding bfs template question 1926

--- algorithms/网络格(DFS-BFS)/BFS/1926.go
+++ algorithms/网络格(DFS-BFS)/BFS/1926.go
@@ -1,1 +1,95 @@
 package BFS
+
+/***
+
+https://leetcode.cn/problems/nearest-exit-from-entrance-in-maze/solutions/869437/go-bfs-by-endlesscheng-k2cu/
+灵神的题解， 学习一下，用作 BFS 的模板。
+
+ */
+
+type pair struct{ x, y int }
+
+var dir4 = []pair{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
+func nearestExit(g [][]byte, entrance []int) int {
+	n, m := len(g), len(g[0])
+	s := pair{entrance[0], entrance[1]}
+	g[s.x][s.y] = 0 // 重置成 0， 省去了 visited 数组。
+	q := []pair{s}
+	for ans := 1; len(q) > 0; ans++ {
+		tmp := q
+		q = nil
+		for _, p := range tmp {
+			for _, d := range dir4 {
+				if x, y := p.x+d.x, p.y+d.y; 0 <= x && x < n && 0 <= y && y < m && g[x][y] == '.' {
+					if x == 0 || y == 0 || x == n-1 || y == m-1 {
+						return ans
+					}
+					g[x][y] = 0
+					q = append(q, pair{x, y})
+				}
+			}
+		}
+	}
+	return -1
+}
+
+func nearestExit(maze [][]byte, entrance []int) int {
+	m := len(maze)
+	n := len(maze[0])
+
+	q := [][2]int{{entrance[0], entrance[1]}}
+	maze[entrance[0]][entrance[1]] = 0 // 原地修改，可以省去 visited 数组。
+	for step := 1; len(q) > 0; step++ {
+		tmp := q
+		q = nil
+		for _, p := range tmp {
+			dirs := [][2]int{{-1, 0}, {0, -1}, {1, 0}, {0, 1}}
+			for _, d := range dirs {
+				r, c := p[0]+d[0], p[1]+d[1]
+				if r >= 0 && r < m && c >= 0 && c < n && maze[r][c] == '.' {
+					if r == 0 || r == m-1 || c == 0 || c == n-1 {
+						return step
+					}
+					q = append(q, [2]int{r, c})
+					maze[r][c] = 0
+					//if p[0] != entrance[0] || p[1] != entrance[1] {
+					//	return step
+					//}
+					//continue
+				}
+			}
+		}
+	}
+	return -1
+}
+
+func nearestExit(maze [][]byte, entrance []int) int {
+	m := len(maze)
+	n := len(maze[0])
+
+	q := [][2]int{{entrance[0], entrance[1]}}
+	maze[entrance[0]][entrance[1]] = 0 // 原地修改，可以省略 visited 数组。
+	for step := 0; len(q) > 0; step++ {
+		tmp := q
+		q = nil
+		for _, p := range tmp {
+			dirs := [][2]int{{-1, 0}, {0, -1}, {1, 0}, {0, 1}}
+			for _, d := range dirs {
+				r, c := p[0]+d[0], p[1]+d[1]
+				if r < 0 || r >= m || c < 0 || c >= n {
+					if p[0] != entrance[0] || p[1] != entrance[1] {
+						return step
+					}
+					continue
+				}
+				// otherwise add to the queue
+				if maze[r][c] == '.' {
+					q = append(q, [2]int{r, c})
+					maze[r][c] = 0
+				}
+			}
+		}
+	}
+	return -1
+}
---
+++ algorithms/网络格(DFS-BFS)/DFS/dfs_template_code.go
@@ -0,0 +1,51 @@
+package DFS
+
+func grid_dfs_template(grid [][]int) int {
+	m := len(grid)
+	n := len(grid[0])
+
+	target := 2
+	// 根据题目的需要， 如果可以 原地修改 grid的值不会乱，那么可以省略 visited 数组。
+	visited := make([][]bool, m)
+	for i := range visited {
+		visited[i] = make([]bool, n)
+	}
+
+	var res bool // 建议使用 global variable 返回 dfs 里的解，建议不要用 dfs 的返回值，容易错, 除非特别方便。
+	var dfs func(int, int)
+
+	dfs = func(x, y int) {
+		if visited[x][y] {
+			return
+		}
+		visited[x][y] = true
+		grid[x][y] = -1 // 如果可以修改 grid 的值，可以替代 visited 数组。
+		dirs := [][2]int{{1, 0}, {-1, 0}, {0, -1}, {0, 1}}
+
+		for _, d := range dirs {
+			r, c := x+d[0], y+d[1]
+			if r < 0 || r >= m || c < 0 || c >= n {
+				res = false // reach border so mark this connected components group whole as false
+				continue
+			}
+			if grid[r][c] == target {
+				dfs(r, c)
+			}
+		}
+	}
+
+	// loop matrix to find answer
+	ans := 0
+	for i := range grid {
+		for j := range grid[0] {
+			if grid[i][j] == target {
+				res = true
+				dfs(i, j)
+				if res {
+					ans++
+				}
+			}
+		}
+	}
+	return ans
+}
