bac73a41f490c3b08a34643afd9faf238ff6f310
张春雷 <zhang_chunlei@hotmail.com>
2024-02-01 12:35:36 +0800

    adding union-find recursive implementation template code

--- data-structures/union-find/union-find.go
+++ data-structures/union-find/union-find.go
@@ -1,51 +1,43 @@
-package leetcode
+package union_find
 
-// recursive 的版本，是路径全压缩的！ loop 版本的 find 不是。
-// 注意了 parent[x] = x 的初始化很重要， 要不然在创建 parent 的时候初始化， 要不然， 在 find 里面初始化 parent[x]
-func main() {
-	/*
-			union-find template
-	 */
-	var n int
-	parent := make([]int, n)
-	for i := range parent {
-		parent[i] = i
-	}
-
-	var find func(int) int
-
-	find = func(x int) int {
-		if parent[x] != x {
-			parent[x] = find(parent[x])
-		}
-		return parent[x]
-	}
-
-	union := func(from, to int) {
-		x, y := find(from), find(to)
-		parent[x] = y
-	}
-
+type UnionFind struct {
+	fa       []int
+	sz       []int
+	comp_cnt int
 }
 
-// another implementation example
-
-func equationsPossible(equations []string) {
-	parent := make([]int, 26)
-	for i := 0; i < 26; i++ {
-		parent[i] = i
+func NewUnionFind(n int) *UnionFind {
+	fa := make([]int, n)
+	sz := make([]int, n)
+	for i := range fa {
+		fa[i] = i
+		sz[i] = 1
 	}
-	//....
+	return &UnionFind{fa, sz, n}
 }
 
-func union(parent []int, index1, index2 int) {
-	parent[find(parent, index1)] = find(parent, index2)
+func (u UnionFind) find(x int) int {
+	if u.fa[x] != x {
+		u.fa[x] = u.find(u.fa[x])
+	}
+	return u.fa[x]
 }
 
-func find(parent []int, index int) int {
-	for parent[index] != index {
-		parent[index] = parent[parent[index]]
-		index = parent[index]
+func (u UnionFind) unite(x, y int) {
+	x = u.find(x)
+	y = u.find(y)
+	if x == y {
+		return
 	}
-	return index
+	if u.sz[x] < u.sz[y] {
+		x, y = y, x
+	}
+	u.fa[y] = x
+	u.sz[x] += u.sz[y]
+}
+
+func (u UnionFind) connected(x, y int) bool {
+	x = u.find(x)
+	y = u.find(y)
+	return x == y
 }
