cca9e2e6634a6bc58cfd31d34703d7fdace38c0d
chunlei <zhang_chunlei@hotmail.com>
2024-02-04 20:56:06 +0800

    adding z function template code

---
+++ algorithms/z-algorithm/template_code.go
@@ -0,0 +1,22 @@
+package z_algorithm
+
+/***
+code from endlesscheng
+https://github.com/EndlessCheng/codeforces-go/blob/master/misc/atcoder/abc284/f/f.go#L11
+*/
+
+func z_function(s string) []int {
+	n := len(s)
+	z := make([]int, n)
+
+	for i, l, r := 1, 0, 0; i < n; i++ {
+		if i <= r {
+			z[i] = min(z[i-l], r-i+1)
+		}
+		for i+z[i] < n && s[z[i]] == s[i+z[i]] {
+			l, r = i, i+z[i]
+			z[i]++
+		}
+	}
+	return z
+}
