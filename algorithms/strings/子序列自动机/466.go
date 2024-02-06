f027bff7f26e264fe7312728c80753ad16613476
chunlei <zhang_chunlei@hotmail.com>
2024-02-06 21:33:52 +0800

    adding 466

--- algorithms/strings/子序列自动机/466.go
+++ algorithms/strings/子序列自动机/466.go
@@ -33,6 +33,51 @@ s1 和 s2 由小写英文字母组成
 
  */
 
+/***
+灵神的版本， 处理， pattern repeat 的时候，有问题。 还是用自己的版本吧。
+ */
+func getMaxRepetitions(s1 string, n1 int, s2 string, n2 int) int {
+	// build nxt
+	// nxt[i][j] 表示在i的右侧, 字符 j (第一次出现）的最近位置
+	s := s1
+	pos := [26]int{}
+	for i := range pos {
+		pos[i] = len(s) + 2
+	}
+	nxt := make([][26]int, len(s)+1)
+	for i := len(s) - 1; i >= 0; i-- {
+		nxt[i+1] = pos
+		pos[s[i]-'a'] = i + 1
+	}
+	nxt[0] = pos // 灵神的版本 是没有保存最后一个 pos的, 处理的方法，留在了 match function 去比较一下 t[0] == s[0]
+
+	match := func(t string) int{
+		cnts1, cnts2 := 0, 0
+		i, j := 0, 0
+		for true {
+			for ; j < len(s2); j++ {
+				i = nxt[i][s2[j]-'a']
+				if i == len(s)+2 {
+					i = 0
+					cnts1++
+					if cnts1 >= n1 {
+						return cnts2
+					}
+					break
+				}
+			}
+			if j == len(s2) {
+				cnts2++
+				j=0
+			}
+		}
+		return 0
+	}
+	l :=match(s2)
+	return l/n2
+}
+
+
 func getMaxRepetitions(s1 string, n1 int, s2 string, n2 int) int {
 	// build nxt
 	// nxt[i][j] 表示在i的右侧, 字符 j (第一次出现）的最近位置
@@ -87,69 +132,3 @@ func getMaxRepetitions(s1 string, n1 int, s2 string, n2 int) int {
 	return l
 }
 
-
-//
-//func getMaxRepetitions(s1 string, n1 int, s2 string, n2 int) int {
-//	// build nxt
-//	// nxt[i][j] 表示在i的右侧, 字符 j (第一次出现）的最近位置
-//	s := s1
-//	pos := [26]int{}
-//	for i := range pos {
-//		pos[i] = len(s) // 初始化， mark end, 在 match 的时候会判断使用
-//	}
-//	nxt := make([][26]int, len(s))
-//
-//	for i := len(s) - 1; i >= 0; i-- {
-//		nxt[i] = pos
-//		pos[s[i]-'a'] = i // 其实是为了 nxt[i-1] 准备的 pos 值。
-//		// 这样写边界处理的很清晰， 但是有一个问题就是 nxt[0] = pos[s[1]-'a'], 我们发现 没有 nxt[-1] 也就是说
-//		// pos[s[0]-'a'] 的这个 match 关系我们没有保存，是丢掉了的。针对这个问题，就需要在 match 里对 s[0] 特殊处理。
-//	}
-//
-//	match := func(t string, m int) bool {
-//		cnts1, cnts2 := 0, 0
-//		i, j := 0, 0
-//
-//		for true {
-//			//// 因为我们在 build nxt 的时候，丢弃了 pos[s[0]-'a'] 这个信息，所以需要在这里，特殊处理一下。
-//			if i == 0 {
-//				if t[j] == s[i] {
-//					j = j + 1 // t[0] 匹配。
-//				}
-//			}
-//
-//			for ; j < len(t); j++ {
-//				i = nxt[i][t[j]-'a']
-//				if i == len(s) {
-//					// 很多题，可以在这里处理，想到到的结果。
-//					i = 0 // reset from beginning
-//					cnts1++
-//					if cnts1 >= n1 {
-//						return false
-//					}
-//					break
-//				}
-//			}
-//			if j == len(t) {
-//				//found a match
-//				j = 0 //reset to beginning
-//				cnts2++
-//				if cnts2 >= m {
-//					return true
-//				}
-//			}
-//		}
-//		return false
-//	}
-//
-//	l, r := 0, len(s1)*n1/len(s2)+1
-//	for l+1 < r {
-//		mid := (l + r) >> 1
-//		if match(s2, mid*n2) {
-//			l = mid
-//		} else {
-//			r = mid
-//		}
-//	}
-//	return l
-//}
