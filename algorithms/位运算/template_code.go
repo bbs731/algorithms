a937c00c7373a88a9c67f6f393ed5d5482080b7b
张春雷 <zhang_chunlei@hotmail.com>
2024-02-07 11:53:51 +0800

    adding bits template code

--- algorithms/位运算/template_code.go
+++ algorithms/位运算/template_code.go
@@ -24,10 +24,28 @@ func _() {
 	//二进制最低 1 及其后面的 0 叫做 lowbit
 	lowbit := s & (-s) // lowbit == 1<<bits.TrailingZeros(s)  除了 s=0 的情况
 
-	//遍历集合：
+	//遍历集合： 假设元素包括 从 0 到 n-1 个元素
 	for i := 0; i < n; i++ {
 		if s>>i&1 == 1 { // i 在 s 中
 			// 处理 i 的逻辑
+		}
+	}
+
+	//枚举集合
+	for s := 0; s < 1<<n; s++ {
+		// 处理s 的逻辑, s 是一个集合里面的元素范围是 0 到 n-1
+	}
+
+	//从大到小枚举集合 s 的所有非空子集 sub:
+	for sub := s; sub > 0; sub = (sub - 1) & s { // sub = (sub-1)&s 这个有深意，看上面灵神链接里面的解释
+		// 处理 子集 sub
+	}
+	//从大到小枚举集合 s 的所有子集sub 包括空集的情况：
+	for sub := s; ; {
+		// 处理子集 sub
+		sub = (sub - 1) & s
+		if sub == s {
+			break
 		}
 	}
 
