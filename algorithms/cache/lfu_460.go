8450507e5007375170ff1f729733564e3c3478f2
张春雷 <zhang_chunlei@hotmail.com>
2024-02-08 17:19:34 +0800

    chores

--- algorithms/cache/lfu_460.go
+++ algorithms/cache/lfu_460.go
@@ -4,6 +4,7 @@ import "container/list"
 
 /***
 https://leetcode.cn/problems/lfu-cache/solutions/2457716/tu-jie-yi-zhang-tu-miao-dong-lfupythonja-f56h/
+条理清楚，太牛了！
  */
 type entry struct {
 	key, value, freq int // freq 表示这本书被看的次数
@@ -45,7 +46,7 @@ func (c *LFUCache) getEntry(key int) *entry {
 			c.minFreq++
 		}
 	}
-	e.freq++ // 看书次数 +1
+	e.freq++       // 看书次数 +1
 	c.pushFront(e) // 放在右边这摞书的最上面
 	return e
 }
@@ -63,7 +64,7 @@ func (c *LFUCache) Put(key, value int) {
 		return
 	}
 	if len(c.keyToNode) == c.capacity { // 书太多了
-		lst := c.freqToList[c.minFreq] // 最左边那摞书
+		lst := c.freqToList[c.minFreq]                           // 最左边那摞书
 		delete(c.keyToNode, lst.Remove(lst.Back()).(*entry).key) // 移除这摞书的最下面的书
 		if lst.Len() == 0 { // 这摞书是空的
 			delete(c.freqToList, c.minFreq) // 移除空链表
@@ -72,4 +73,3 @@ func (c *LFUCache) Put(key, value int) {
 	c.pushFront(&entry{key, value, 1}) // 新书放在「看过 1 次」的最上面
 	c.minFreq = 1
 }
-
