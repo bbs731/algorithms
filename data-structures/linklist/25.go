60ed3e92a9db3812ae5a0a1ff5c509a2b27eab05
张春雷 <zhang_chunlei@hotmail.com>
2024-02-09 10:48:44 +0800

    adding 92

---
+++ data-structures/linklist/25.go
@@ -0,0 +1,12 @@
+package linklist
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseKGroup(head *ListNode, k int) *ListNode {
+
+}
