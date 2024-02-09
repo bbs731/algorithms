6841ae70d42a1f2f5bffa944a9dd8fd5a170fa05
张春雷 <zhang_chunlei@hotmail.com>
2024-02-09 11:38:11 +0800

    chores

--- data-structures/linklist/25.go
+++ data-structures/linklist/25.go
@@ -36,10 +36,40 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 
 		// 见视频
 		p0.Next.Next = cur
-		tmp := p0.Next  // 这个太重要了，想想，为什么？
+		tmp := p0.Next // 这个太重要了，想想，为什么？
 		p0.Next = pre
 		p0 = tmp
 	}
 
+	return dummy.Next
+}
+
+/***
+https://leetcode.cn/problems/reverse-nodes-in-k-group/solutions/1992228/you-xie-cuo-liao-yi-ge-shi-pin-jiang-tou-plfs/
+灵神的答案，真是简明啊！
+ */
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	n := 0
+	for cur := head; cur != nil; cur = cur.Next {
+		n++ // 统计节点个数
+	}
+
+	dummy := &ListNode{Next: head}
+	p0 := dummy
+	var pre, cur *ListNode = nil, p0.Next
+	for ; n >= k; n -= k {
+		for i := 0; i < k; i++ {
+			nxt := cur.Next
+			cur.Next = pre // 每次循环只修改一个 Next，方便大家理解
+			pre = cur
+			cur = nxt
+		}
+
+		// 见视频
+		nxt := p0.Next
+		p0.Next.Next = cur
+		p0.Next = pre
+		p0 = nxt
+	}
 	return dummy.Next
 }
