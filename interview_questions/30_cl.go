076406b4c975eed842de5467ba5744f52a2a5f40
chunlei <zhang_chunlei@hotmail.com>
2024-02-15 14:18:47 +0800

    comments

--- interview_questions/30_cl.go
+++ interview_questions/30_cl.go
@@ -6,6 +6,7 @@ package interview_questions
 chunlei 你和牛，不要被眼前的挫折所累， 没关系， 坚持下去，证明你的想好好多就是对的，但是表达的不够高明。
 向高手学习，进步终有一天你会和他们一样，站在山顶
  */
+
 func findSubstring(s string, words []string) []int {
 	n := len(words[0])
 	wordm := make(map[string]int)
@@ -15,21 +16,20 @@ func findSubstring(s string, words []string) []int {
 	m := len(wordm)
 	ans := []int{}
 
+	// loop 里面， 套一层 sliding window
 	for j:=0;  j<n; j++ {
 		left := j
 		counter := make(map[string]int)
 		// 枚举 right edge of window
-		for i :=left; i <= len(s)-n; {
+		for i :=left; i <= len(s)-n; i+=n{
 			next := s[i : i+n]
 			if _, ok := wordm[next]; !ok {
-				i +=n
-				left = i
+				left = i+n
 				// clear counter
 				counter = make(map[string]int)
 				continue
 			}
 			counter[next]++
-			i += n
 			for counter[next] > wordm[next] {
 				// move left edge of window
 				leftword := s[left : left+n]
