45418f57906a5472045cdede8d763570e97be5be
chunlei <zhang_chunlei@hotmail.com>
2024-02-16 22:12:32 +0800

    chores

---
+++ data-structures/Heap/3013_todo.go
@@ -0,0 +1,11 @@
+package Heap
+
+
+/***
+https://www.bilibili.com/video/BV1oV411D7gB/?spm_id_from=333.999.0.0&vd_source=84c3c489cf545fafdbeb3b3a6cd6a112
+用 lazy heap 解一下
+
+golang 里面的 ordered dictionary 就是 redbalck tree 的实现
+https://github.com/emirpasic/gods?tab=readme-ov-file#redblacktree
+看 gods 的实现
+ */
