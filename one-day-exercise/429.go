f2d9c7715c48a579fe32550a507809254fea4288
chunlei <zhang_chunlei@hotmail.com>
2024-02-17 11:53:11 +0800

    429

--- one-day-exercise/429.go
+++ one-day-exercise/429.go
@@ -1,1 +1,31 @@
 package one_day_exercise
+
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func levelOrder(root *Node) [][]int {
+	if root == nil {
+		return  nil
+	}
+	q := []*Node{root}
+	ans := make([][]int, 0)
+
+	for len(q) > 0 {
+		tmp := q
+		q = nil
+
+		level := make([]int, 0, len(tmp))
+		for _, n := range tmp {
+			level = append(level, n.Val)
+			q = append(q, n.Children...)
+		}
+		ans = append(ans, level)
+	}
+	return ans
+}
--- one-day-exercise/common.go
+++ one-day-exercise/common.go
@@ -1,1 +1,8 @@
 package one_day_exercise
+
+
+type Node struct {
+	Val int
+	Children []*Node
+}
+
