7843057656dd18baa78b482be48b06e71bb307b1
张春雷 <zhang_chunlei@hotmail.com>
2024-02-19 15:24:18 +0800

    chores

---
+++ data-structures/Trie/3045_todo.go
@@ -0,0 +1,8 @@
+package Trie
+
+/***
+字典树的巧妙运用
+
+video 讲解：  https://www.bilibili.com/video/BV1jZ42127Yf/?spm_id_from=333.1368.list.card_archive.click&vd_source=84c3c489cf545fafdbeb3b3a6cd6a112
+题解： https://leetcode.cn/problems/count-prefix-and-suffix-pairs-ii/solutions/2644160/z-han-shu-zi-dian-shu-pythonjavacgo-by-e-5c2v/
+ */
