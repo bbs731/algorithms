88dc7521078ef82ecfe4f5d28f1ed318c421bc2c
张春雷 <zhang_chunlei@hotmail.com>
2024-02-27 16:55:28 +0800

    BIT

--- data-structures/binaryIndexTree/LC307.go
+++ data-structures/binaryIndexTree/LC307.go
@@ -8,23 +8,56 @@ https://leetcode.cn/problems/range-sum-query-mutable/solutions/2524481/dai-ni-fa
  */
 
 type NumArray struct {
+	original []int
+	b        []int
+	n        int
 }
 
 func Constructor(nums []int) NumArray {
+	n := len(nums)
+	b := make([]int, n+1)
 
+	// construct BIT in O(n) time  // 这里不熟悉， 容易出问题。
+	for i := 0; i < n; i++ {
+		b[i+1] += nums[i] // 这里有bug 需要 +=,   = 是不对的，需要累加
+		c := i + 1 + lowbit(i+1)
+		if c <= n {
+			b[c] += b[i+1] // update its parent, 需要 +=  child b[i+1] 而不是 num[i]
+		}
+	}
+
+	return NumArray{
+		nums,
+		b,
+		n,
+	}
+}
+
+func lowbit(x int) int {
+	return x & (-x)
 }
 
 func (this *NumArray) Update(index int, val int) {
+	v := val - this.original[index]
 
+	x := index + 1
+	for x <= this.n {
+		this.b[x] += v
+		x += lowbit(x)
+	}
+
+	// update original string
+	this.original[index] = val
 }
 
+func (this *NumArray) query(x int) int {
+	ans := 0
+	for x >= 1 {
+		ans += this.b[x]
+		x -= lowbit(x)
+	}
+	return ans
+}
 func (this *NumArray) SumRange(left int, right int) int {
-
+	return this.query(right+1) - this.query(left)
 }
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * obj := Constructor(nums);
- * obj.Update(index,val);
- * param_2 := obj.SumRange(left,right);
- */
