57995a66dc24b09186d59788cb53d664f970acab
chunlei <zhang_chunlei@hotmail.com>
2024-03-03 12:54:37 +0800

    chores

--- weekly/109/2786_1.go
+++ weekly/109/2786_1.go
@@ -59,3 +59,22 @@ func maxScore(nums []int, x int) int64 {
 	// 这道题，犯错，来这这里。 需要返回的是不是 f0
 	return int64(f[nums[0]%2])
 }
+
+func maxScore(nums []int, x int) int64 {
+	// f[i][j] = max( f[i+1][j] + nums[i], f[i+1][j^1] + nums[i] -x ) if j == nums[i] %2
+	n := len(nums)
+	f := [2]int{}
+
+	for i := n-1; i>=0; i-- {
+		for j:=0; j<2; j++ {
+			v := nums[i]
+			//if nums[i] %2 == j {
+			// 因为 j == v %2, 所以把所有的 j 替换成  v%2
+			f[v%2] = max(f[v%2]+nums[i], f[v%2^1]+nums[i]-x)
+			//}
+		}
+	}
+	// 这道题，犯错，来这这里。 需要返回的是不是 f0
+	return int64(f[nums[0]%2])
+}
+
