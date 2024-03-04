a866677e0818acd6dc4b53f48af791f7991450b9
张春雷 <zhang_chunlei@hotmail.com>
2024-03-04 17:49:53 +0800

    adding biparite tempalte code for 886

--- algorithms/graphs/Bipartite Graph/886.go
+++ algorithms/graphs/Bipartite Graph/886.go
@@ -1,5 +1,36 @@
 package Bipartite_Graph
 
 func possibleBipartition(n int, dislikes [][]int) bool {
+	g := make([][]int, n)
 
+	for _, d := range dislikes {
+		v, w := d[0]-1, d[1]-1 // index 是从 1 开始算的额，所以可以减去1
+		g[v] = append(g[v], w)
+		g[w] = append(g[w], v)
+	}
+
+	colors := make([]int, n)
+
+	var dfs func(int, int) bool
+	dfs = func(v int, c int) bool {
+		colors[v] = c
+
+		for _, w := range g[v] {
+			if colors[w] == c || colors[w] == 0 && !dfs(w, 3^c) {
+				return false
+			}
+		}
+		return true
+	}
+
+	for i, c := range colors {
+		if c != 0 {
+			continue
+		}
+
+		if !dfs(i, 1) {
+			return false
+		}
+	}
+	return true
 }
