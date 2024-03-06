8ab6b212368bf321d38a382bedc15ef7812b96a1
张春雷 <zhang_chunlei@hotmail.com>
2024-03-06 15:26:22 +0800

    75

--- algorithms/quick-select/75.go
+++ algorithms/quick-select/75.go
@@ -1,9 +1,8 @@
 package quick_select
 
-
 func sortColors(nums []int) {
-	n :=len(nums)
-	left, right :=0, n-1
+	n := len(nums)
+	left, right := 0, n-1
 	i := 0
 	/***
 	题解：  https://leetcode.cn/problems/sort-colors/solutions/2651983/yi-kan-jiu-dong-de-dai-ma-by-bo-bo-t1-jlht/
@@ -19,12 +18,38 @@ func sortColors(nums []int) {
 		} else if x == 0 {
 			nums[left], nums[i] = nums[i], nums[left]
 			left++ // left 指向的位置是 1
-			i++   // 所以 i 需要 ++
+			i++    // 所以 i 需要 ++
 		} else {
 			// x == 1
 			i++
 		}
 	}
-
 }
 
+func sortColors(nums []int) {
+	n := len(nums)
+	l, r := 0, n-1
+	// invariant  <l is 0  and >r is 2
+
+	for i := 0; i < n; {
+		c := nums[i]
+		if c == 1 || i < l {
+			// all zeros 所以直接 i++
+			i++
+			continue
+		}
+		if i > r {
+			// now all 2s, can break now
+			break
+		}
+
+		// 不能更新 i, 因为 swap 的 nums[l], nums[r] 现在变成了 nums[i], nums[i]的值是什么不确定， 需要再次被loop处理。
+		if c == 0 {
+			nums[l], nums[i] = nums[i], nums[l]
+			l++
+		} else {
+			nums[r], nums[i] = nums[i], nums[r]
+			r--
+		}
+	}
+}
